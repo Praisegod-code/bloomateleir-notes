cebe17f10b86608c17f89c9f51a372e4275f9b9d
praisegod-code <brightpraisegod966@gmail.com>
2026-07-21 10:48:08 +0100

    add STATIC_ROOT for whitenoise

--- note_app_project/note_app_project/settings.py
+++ note_app_project/note_app_project/settings.py
@@ -127,9 +127,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR.parent / "static",
 ]
+
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'   # add this line
 
 STORAGES = {
     "staticfiles": {
