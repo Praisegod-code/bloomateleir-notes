d72af0dc0ee03884986bf7082e5aeb4925c6c3fc
praisegod-code <brightpraisegod966@gmail.com>
2026-07-24 13:30:36 +0100

    Fix DJANGO_SETTINGS_MODULE for nested project structure

--- note_app_project/note_app_project/wsgi.py
+++ note_app_project/note_app_project/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_app_project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_app_project.note_app_project.settings')
 application = get_wsgi_application()
